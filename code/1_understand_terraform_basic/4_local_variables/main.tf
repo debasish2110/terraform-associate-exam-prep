6d3267ed3b856a0c9325411594a93d531aee5a00
debasish2110 <debasishdash98@gmail.com>
2025-05-31 23:47:57 +0530

    local variables added

---
+++ code/1_understand_terraform_basic/4_local_variables/main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+variable "variables_sub_az" {
+  description = "Availability Zone used Variables Subnet"
+  type        = string
+  default = "ap-south-1a"
+}
+
+variable "environment" {
+  description = "environment"
+  type = string
+  default = "dev"
+}
+
+locals {
+  team = "api-mgmt-dev"
+  application = "corp_api"
+  server_name = "ec2-${var.environment}-api-${var.variables_sub_az}"
+}
+
+
+resource "aws_instance" "web_server" {
+  ami = "xxxx"
+  instance_type = "t2.micro"
+  subnet_id = "xxxxx"
+  tags = {
+    Name = local.server_name
+    Owner = local.team
+    App = local.application
+  }
+}

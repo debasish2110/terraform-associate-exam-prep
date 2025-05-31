89c8e1f1dc1409ec1c747c9acee2929b99ecf7b6
debasish2110 <debasishdash98@gmail.com>
2025-06-01 01:05:59 +0530

    Data block added

--- code/1_understand_terraform_basic/5_data_blocks/main.tf
+++ code/1_understand_terraform_basic/5_data_blocks/main.tf
@@ -20,11 +20,19 @@ locals {
   server_name = "ec2-${var.environment}-api-${var.variables_sub_az}"
 }
 
+################################DATA BLOCK#####################################
+data "aws_region" "current" {}
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+###############################################################################
 
 resource "aws_instance" "web_server" {
   ami           = "xxxx"
   instance_type = "t2.micro"
   subnet_id     = "xxxxx"
+  availability_zone = tolist(data.aws_availability_zones.available.names)[each.value]
+  
   tags = {
     Name  = local.server_name
     Owner = local.team
